6ef185eff59be48e32150d5be7a6dd6804de456a
Gabriela Vilela <gabrielathalitasv@gmail.com>
2020-03-06 14:59:43 -0300

    Sharing a service with 2 components

---
+++ Angular 2 - Loiane/servicos/src/app/cursos/cursos.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class CursosService {
+
+    constructor() {
+        console.log('CursosService')
+    }
+    getCursos() {
+        return ['Angular 2', 'Java', 'React', 'C#']
+    }
+}
